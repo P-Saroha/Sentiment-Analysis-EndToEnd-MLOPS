31930871b1d69977c88f8a08680f4e5db09e37df
Parveen Saroha <parveensaroha2002@gmail.com>
2025-09-26 10:53:39 +0530

    project folder structure is ready

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='this is end to end sentiment analysis project',
+    author='parveen saroha',
+    license='MIT',
+)
